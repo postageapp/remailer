f23ca65a5437958b6ab0476610f030efd4cc4af7
Scott Tadman <scott@twg.ca>
2010-12-02 15:05:07 -0500

    Adding sample configuration file for testing

---
+++ test/config.example.rb
@@ -0,0 +1,17 @@
+TestConfig.smtp_server = {
+  :host => "smtp.example.com",
+  :identifier => "smtp.example.com"
+} 
+
+TestConfig.public_smtp_server = {
+  :host => "smtp.gmail.com",
+  :identifier => "mx.google.com",
+  :username => "--your--username--gmail.com",
+  :password => "--your--password--",
+  :port => 587
+}
+
+TestConfig.proxy_server = "proxy.example.com"
+
+TestConfig.recipient = "--your--email--"
+TestConfig.sender = "--your--email--"
