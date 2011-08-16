f61c21ca7f030f91bbdc711c091e5ad8d634d15e
Scott Tadman <scott@twg.ca>
2011-08-16 17:41:53 -0400

    Switch to :established state after secondary HELO

--- lib/remailer/connection/smtp_interpreter.rb
+++ lib/remailer/connection/smtp_interpreter.rb
@@ -196,11 +196,11 @@ class Remailer::Connection::SmtpInterpreter < Remailer::Interpreter
     
     interpret(220) do
       # "HELO/EHLO command already issued"
-      enter_state(:mail_from)
+      enter_state(:established)
     end
     
     interpret(250) do
-      enter_state(:mail_from)
+      enter_state(:established)
     end
   end
   
